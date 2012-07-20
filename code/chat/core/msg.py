ed586f982d3db94c6081dbea3e9b91c9f8683993
Yun <onelinecloud@gmail.com>
2012-07-20 11:49:01 +0800

    chat core no need msg manage
    Because message and notice manage based on Session's data structure.

--- code/chat/core/msg.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-import copy
-import gevent
-from core.session import SessionManager
-from core.user import UserManager
-from core.default import WAIT_TIMEOUT
-from settings import WAIT_DELAY
