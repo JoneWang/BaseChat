1e51dd7b098a75904519360aee497ad3d7bf3ec6
Yun <onelinecloud@gmail.com>
2012-07-20 17:51:25 +0800

    simplify current_time function

--- code/chat/lib/common.py
+++ code/chat/lib/common.py
@@ -4,10 +4,9 @@ import datetime
 from chat.lib.relativedelta import relativedelta
 
 def current_time(format='%Y-%m-%d %H:%M:%S', **interval):
+    obj_time = *time.localtime()[:6])
     if interval:
-        obj_time = *time.localtime()[:6]) + relativedelta(**interval)
-    else:
-        obj_time = *time.localtime()[:6])
+        obj_time += relativedelta(**interval)
 
     return str(time.strftime(format,
         (datetime.datetime(obj_time).timetuple()))
